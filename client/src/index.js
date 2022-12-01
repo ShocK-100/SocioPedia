1f3a65dc4c315112382fa781a7f5666810c089bd
ShocK-100 <talzvi99@gmail.com>
2022-12-01 09:51:04 +0200

    mui settings created

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import authReducer from "./state"; //good import?
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import {
   persistStore,
@@ -16,7 +16,6 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { persistGate } from "redux-persist/integration/react";
 import { PersistGate } from "redux-persist/integration/react";
 
 const persistConfig = { key: "root", storage, version: 1 };
